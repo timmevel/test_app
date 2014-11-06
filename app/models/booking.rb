93c4166b20868a9865d42d26cfea4ad1ac0e6081
timmevel <t.mev@hotmail.fr>
2014-11-07 00:41:45 +0100

    modif img src

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,12 @@
 class Booking < ActiveRecord::Base
   belongs_to :show
+  #before_save :check_capacity
+  #after_save :update_capacity
+
+
+
+  #def update_capacity
+  #  show.capacity = show.capacity - number
+  #  show.save
+  #end
 end
